6de37242ff616efbf764a08d8308ac7dbfc4ac68
ali <alisafavi123@gmail.com>
2022-02-19 13:24:13 +0330

    Initial commit

---
+++ app/src/main/java/aliSafavi/Check/MainActivity.kt
@@ -0,0 +1,11 @@
+package aliSafavi.Check
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
