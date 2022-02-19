e4cd99f89edc67b9fcd382a9f06d9560106f8450
ali <alisafavi123@gmail.com>
2022-02-20 00:00:31 +0330

    Entities & Models & nav_graph

--- app/src/main/java/aliSafavi/Check/MainActivity.kt
+++ app/src/main/java/aliSafavi/Check/MainActivity.kt
@@ -1,7 +1,12 @@
 package aliSafavi.Check
 
+import aliSafavi.Check.data.AppDatabase
+import aliSafavi.Check.model.Bank
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
